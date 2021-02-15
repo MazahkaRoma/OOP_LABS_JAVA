b46fe9d42e88329ccc8967161e89ff35a78c9880
MZH <mazahkaroma@mail.ru>
2021-02-15 23:02:06 +0300

    Законченно первое задание

--- src/OOP_Labs/Main.java
+++ src/OOP_Labs/Main.java
@@ -2,11 +2,26 @@ package OOP_Labs;
 
 public class Main
 {
-    public static void main(String[] args) {
-
-    }
-    public static boolean isPrimal(int number, int n)
+    public static void main(String[] args)
     {
-        return false;
+        System.out.print("Простые числа от 0 до 100: ");
+        for (int i=2;i<=100;i++)
+        {
+            if(isPrime(i))
+            {
+                System.out.print(i+", ");
+            }
+        }
+    }
+    public static boolean isPrime(int n) //Является ли чсло простым
+    {
+        for(int i=2;i<n;i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
