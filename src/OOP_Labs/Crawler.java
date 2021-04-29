08f56bd683285dd57b5e4f6f88c24b74a288a605
MZH <mazahkaroma@mail.ru>
2021-04-29 09:09:29 +0300

    Lab 8 Done

--- src/OOP_Labs/Crawler.java
+++ src/OOP_Labs/Crawler.java
@@ -1,78 +1,49 @@
 package OOP_Labs;
 
-
-import java.io.*;
-import java.net.*;
 import java.util.LinkedList;
 
-    public class Crawler {
+public class Crawler {
+    public static void showResult(LinkedList<URLDepthPair> resultLink) {
+        for (URLDepthPair c : resultLink)
+            System.out.println("Depth :" + c.getDepth()+"\tLink :"+c.getURL());
+    }
 
-        static LinkedList <URLDepthPair> findLink = new LinkedList <URLDepthPair>();
-        static LinkedList <URLDepthPair> viewedLink = new LinkedList <URLDepthPair>();
+    public static boolean checkDigit(String line) {
+        boolean isDigit = true;
+        for (int i = 0; i < line.length() && isDigit; i++)
+            isDigit = Character.isDigit(line.charAt(i));
+        return isDigit;
+    }
 
+    public static void main(String[] args) {
+        args = new String[]{"https://natribu.org/ru", "10", "10"};
+        if (args.length == 3&&checkDigit(args[1])&&checkDigit(args[2])) {
+            String lineUrl = args[0];
+            int numThreads = Integer.parseInt(args[2]);
+            URLPool pool = new URLPool(Integer.parseInt(args[1]));
+            pool.addPair(new URLDepthPair(lineUrl, 0));
+            for (int i = 0; i < numThreads; i++) {
+                CrawlerTask c = new CrawlerTask(pool);
+                Thread t = new Thread(c);
+                t.start();
+            }
 
-        public static void showResult(LinkedList<URLDepthPair> viewedLink) {
-            for (URLDepthPair c : viewedLink)
-                System.out.println("Depth : "+c.getDepth() + "\tLink : "+c.getURL());
-        }
-
-
-        public static void request(PrintWriter out,URLDepthPair pair) throws MalformedURLException {
-            out.println("GET " + pair.getPath() + " HTTP/1.1");
-            out.println("Host: " + pair.getHost());
-            out.println("Connection: close");
-            out.println();
-            out.flush();
-        }
-
-        public static void Process(String pair, int maxDepth) throws IOException {
-            findLink.add(new URLDepthPair(pair, 0));
-            while (!findLink.isEmpty()) {
-                URLDepthPair currentPair = findLink.removeFirst();
-                if (currentPair.depth < maxDepth) {
-                    Socket my_socket = new Socket(currentPair.getHost(), 80);
-                    my_socket.setSoTimeout(1000);
-                    try {
-                        BufferedReader in = new BufferedReader(new InputStreamReader(my_socket.getInputStream()));
-                        PrintWriter out = new PrintWriter(my_socket.getOutputStream(), true);
-                        request(out, currentPair);
-                        String line;
-                        while ((line = in.readLine()) != null) {
-                            if (line.indexOf(currentPair.URL_PREFIX) != -1 && line.indexOf('"') != -1) {
-                                StringBuilder currentLink = new StringBuilder();
-                                int i = line.indexOf(currentPair.URL_PREFIX);
-                                while (line.charAt(i) != '"' && line.charAt(i) != ' ') {
-                                    if (line.charAt(i) == '<') {
-                                        currentLink.deleteCharAt(currentLink.length() - 1);
-                                        break;
-                                    }
-                                    else {
-                                        currentLink.append(line.charAt(i));
-                                        i++;
-                                    }
-                                }
-                                URLDepthPair newPair = new URLDepthPair(currentLink.toString(), currentPair.depth + 1);
-                                if (currentPair.check(findLink, newPair) && currentPair.check(viewedLink, newPair) && !currentPair.URL.equals(newPair.URL))
-                                    findLink.add(newPair);
-                            }
-                        }
-                        my_socket.close();
-                    } catch (SocketTimeoutException e) {
-                        my_socket.close();
-                    }
+            while (pool.getWait() != numThreads) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    System.out.println("Ignoring  InterruptedException");
                 }
-
-                viewedLink.add(currentPair);
             }
-            showResult(viewedLink);
-        }
-        public static void main(String[] args) {
-            String[] arg = new String[]{"https://vk.com","4"};
             try {
-                Process(arg[0], Integer.parseInt(arg[1]));
-            } catch (NumberFormatException | IOException e) {
-                System.out.println("usage: java crawler " + arg[0] + " " + arg[1]);
+                showResult(pool.getResult());;
+            } catch (NullPointerException e) {
+                System.out.println("Not Link");
             }
+            System.exit(0);
+        } else {
+            System.out.println("usage: java Crawler <URL> <maximum_depth> <num_threads> or second/third not digit");
         }
     }
 
+}
